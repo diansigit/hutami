82827ad5b3a28b626282426093c15a52e38562ee
Dian Sigit <diansigit.p@gmail.com>
2015-07-06 14:48:46 +0700

    Update Screenshot

--- index.php
+++ index.php
@@ -44,9 +44,9 @@ get_header(); ?>
 
 						// Previous/next page navigation.
 						the_posts_pagination( array(
-							'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-							'next_text'          => __( 'Next page', 'twentyfifteen' ),
-							'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
+							'prev_text'          => __( 'Previous page', 'hutami_theme' ),
+							'next_text'          => __( 'Next page', 'hutami_theme' ),
+							'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'hutami_theme' ) . ' </span>',
 						) );
 
 					// If no content, include the "No posts found" template.
